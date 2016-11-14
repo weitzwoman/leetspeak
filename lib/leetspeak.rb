a3c1f0294c7736c873b4c51e37c99d6351244226
Luke Philips and Tracie Weitzman <luke@>
2016-11-14 14:00:08 -0800

    passes all tests

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -2,10 +2,19 @@ class String
   define_method(:leetspeak) do
     newString = ""
     self.each_char() do |letter|
-      if letter == "e"
+      if letter.eql?("e")
         newString<<("3")
+      elsif letter.eql?("o")
+        newString<<("0")
+      elsif letter.eql?("I")
+        newString<<("1")
+      elsif letter.eql?("s")
+        newString<<("z")
       else newString<<(letter)
       end
+    end
+    if newString.start_with?("z")
+      newString[0] = "S"
     end
     newString
   end
--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -7,6 +7,18 @@ describe('String#leetspeak') do
   #   expect("hello world".leetspeak()).to(eq("hello world"))
   # end
   it('replaces every "e" in a string with a "3"') do
-    expect("easement".leetspeak).to(eq("3as3m3nt"))
+    expect("easement".leetspeak).to(eq("3az3m3nt"))
+  end
+  it('replaces every "o" in a string with a "0"') do
+    expect("boo boo".leetspeak).to(eq("b00 b00"))
+  end
+  it('replaces every "I" in a string with a "1"') do
+    expect("I like Ike".leetspeak).to(eq("1 lik3 1k3"))
+  end
+  it('replaces every "s" in a string with a "z"') do
+    expect("roses".leetspeak).to(eq("r0z3z"))
+  end
+  it('does NOT replace the first letter when it is an s') do
+    expect("sassafrass".leetspeak).to(eq("Sazzafrazz"))
   end
 end
